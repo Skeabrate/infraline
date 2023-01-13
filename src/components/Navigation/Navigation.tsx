cc1f99d3d10923cf3b1ee4db2204810279ce9b8b
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-01-13 14:58:54 +0100

    Navigation - styled links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,6 +5,17 @@ import TelephoneSvg from 'assets/svg/TelephoneSvg';
 import Link from 'next/link';
 import ArrowSvg from 'assets/svg/ArrowSvg';
 
+const StyledLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  return (
+    <Link
+      className='flex items-center gap-2 text-sm text-red-900 hover:text-red-700 transition-all duration-150 font-medium'
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+};
+
 const Navigation = () => {
   return (
     <nav className='border-t-4 border-red-700'>
@@ -16,23 +27,17 @@ const Navigation = () => {
       >
         <li className='flex justify-center gap-8 mt-6 row-start-3 md:flex-col md:gap-0 md:row-start-1 md:mt-0'>
           <p>
-            <Link
-              className='flex items-center gap-2 text-sm text-red-900 hover:text-red-700 transition-all duration-150 font-medium'
-              href='mailto:biuro@infraline.pl'
-            >
+            <StyledLink href='mailto:biuro@infraline.pl'>
               <EmailSvg />
               <span className='block md:animate-fadeInTop md:opacity-0'>biuro@infraline.pl</span>
-            </Link>
+            </StyledLink>
           </p>
           <p className='hidden w-52 h-px bg-red-700 my-1 animate-scaleX origin-left scale-x-0 md:block'></p>
           <p>
-            <Link
-              className='flex items-center gap-2 text-sm text-red-900 hover:text-red-700 transition-all duration-150 font-medium'
-              href='tel:+48502135511'
-            >
+            <StyledLink href='tel:+48502135511'>
               <TelephoneSvg />
               <span className='block md:animate-fadeInBottom md:opacity-0'>+48 502 135 511</span>
-            </Link>
+            </StyledLink>
           </p>
         </li>
         <li
